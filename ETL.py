982c1ccda334f75962e63c1574580e3832a409c7
Daniel <0243179@up.edu.mx>
2024-11-25 00:38:07 -0600

    Update ETL

--- ETL.py
+++ ETL.py
@@ -1,6 +1,13 @@
 import polars as pl 
-
 import os 
+import os
+from google.cloud import storage
+from pandas_gbq import to_gbq
+
+
+
+############################ Funtion To Processs all Joins ###################################
+
 def do_joins_to_main(lista_archivos, conjunto_de_datos):
 
     for archivo in lista_archivos:
@@ -14,10 +21,14 @@ def do_joins_to_main(lista_archivos, conjunto_de_datos):
                 print("not working")
                 continue
             else:
-              
-                file_csv = pl.read_csv(f"catalogos/{name_csv[0]}.csv",truncate_ragged_lines=True)
+
+                blob = bucket.blob(f"catalogos/{name_csv[0]}.csv")
+
+                with blob.open("r") as f:
+                    file_csv = pl.read_csv(f,truncate_ragged_lines=True)
+
                 conjunto_de_datos = conjunto_de_datos.join(file_csv, left_on=[column], right_on=["clave"], how="inner")
-              
+            
                 conjunto_de_datos = conjunto_de_datos.with_columns(
                     pl.col("descripción").alias(column)
                 )
@@ -26,25 +37,57 @@ def do_joins_to_main(lista_archivos, conjunto_de_datos):
 
     return  conjunto_de_datos
 
-    
+################################### Main #########################################
 
-conjunto_de_datos = pl.read_csv("conjunto_de_datos/conjunto_de_datos_ed2023.csv")
-
-
-diccionario_de_datos = pl.read_csv("diccionario_de_datos/diccionario_datos_ed2023.csv")
-
-entidades = pl.read_csv("entidad_municipio_localidad_2022.csv").filter((pl.col("cve_mun") == 0) & (pl.col("cve_loc") == 0 ))["cve_ent", "nom_loc"]
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "lrf-bigdata-08d173e4f9bf.json"
 
 
+storage_client = storage.Client()
 
 
+bucket = storage_client.bucket("proyecto_final_divorcios")
+
+
+
+############################ Main Data ###################################
+blob = bucket.blob("conjunto_de_datos/conjunto_de_datos_ed2023.csv")
+with blob.open("r") as f:
+    conjunto_de_datos = pl.read_csv(f)
+
+
+############################ Read Dictionary ###################################
+blob = bucket.blob("diccionario_de_datos/diccionario_datos_ed2023.csv")
+with blob.open("r") as f:
+    diccionario_de_datos = pl.read_csv(f)
+
+
+############################ Read State ###################################
+blob = bucket.blob("entidad_municipio_localidad_2022.csv")
+with blob.open("r") as f:
+    entidades = pl.read_csv(f).filter((pl.col("cve_mun") == 0) & (pl.col("cve_loc") == 0 ))["cve_ent", "nom_loc"]
+
+
+############################ Read all  ###################################
 ruta_carpeta = 'catalogos/'
-lista_archivos = [archivo.replace('.csv', '') for archivo in os.listdir(ruta_carpeta) if os.path.isfile(os.path.join(ruta_carpeta, archivo))]
+blobs = bucket.list_blobs(prefix=ruta_carpeta)
+lista = []
+for blob in blobs:
+    nombre_archivo = os.path.basename(blob.name)
+    lista.append(nombre_archivo)
+
+############################ Replace csv an clean columns  ###################################
+
+lista_archivos = [archivo.replace('.csv', '') for archivo in lista]
 
 lista_columns = ["año_nacimiento", "año_ejecutoria", "dia", "año_sentencia", "año_registro", "edad", "numero_de_hijos", "duracion_matrimonio"]
-# "edad_div1", "nacim_div1", "pat_hij", "cus_hij", "hij_men", "hijos", "anio_eje", "anio_mat", "dia_reg", "anio_reg", "dia_eje", "edad_div2", "nacim_div2", 
+
 for rem in lista_columns:
     lista_archivos.remove(rem)
+
+
+############################ Execute Function And Join to obtain the State  ###################################
+
+
 
 conjunto_de_datos = do_joins_to_main(lista_archivos, conjunto_de_datos).join(entidades, left_on="ent_mat", right_on="cve_ent", how="inner").with_columns(
     pl.col("nom_loc").alias("ent_mat")
@@ -52,4 +95,9 @@ conjunto_de_datos = do_joins_to_main(lista_archivos, conjunto_de_datos).join(ent
 ).drop("nom_loc")
 
 
-print(conjunto_de_datos)
+
+
+conjunto_de_datos = conjunto_de_datos.to_pandas()
+
+
+to_gbq(conjunto_de_datos, destination_table='lrf-bigdata.divorcios.conjunto_datos', project_id='lrf-bigdata', if_exists='replace')
